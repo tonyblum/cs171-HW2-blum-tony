20499c5d2f1c616061a33a3f80ae83bdb30e629c
Tony Blum <blum@college.harvard.edu>
2014-02-28 13:10:40 -0500

    prob 4 progress

---
+++ top_10.c
@@ -0,0 +1,9 @@
+// given a list of 1000 ints find the top 10
+
+// sort the list find the numbers in the last 10 index
+// correct solution, create a helper array of 10 digits,
+// see if the number sort the helper array
+// if the number is bigger than the helper array at index[o], add it
+// sort helper array
+
+int 
